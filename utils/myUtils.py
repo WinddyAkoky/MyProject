cc56af7db8e27d2e36a39f6c88f5c5a974b6e8fb
winddyakoky <winddyakoky@gmail.com>
2020-01-04 19:31:51 +0800

    first commit

---
+++ utils/myUtils.py
@@ -0,0 +1,11 @@
+import os
+import torch
+import torchvision
+import torchvision.transforms as transform
+import numpy as np
+from torch.autograd import Variable
+
+
+def test_accuracy():
+    # 测试模型的准确率
+    pass
